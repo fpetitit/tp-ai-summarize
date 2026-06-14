cd0e572c5d23660621a12f2cd924d7b66be59d80
François Petitit <francois.petitit@gmail.com>
2026-06-14 22:45:04 +0200

    fixup

---
+++ vercel-ai-summary/.next/dev/static/chunks/[project]_vercel-ai-summary_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_next_dist_compiled_14ibvna._.js","static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js","static/chunks/node_modules_next_dist_client_16lnmlo._.js","static/chunks/node_modules_next_dist_0zk-zb1._.js","static/chunks/1qf1_next_dist_pages__error_0f1i7ci.js","static/chunks/[next]_entry_page-loader_ts_02c0ybi._.js","static/chunks/node_modules_react-dom_0kla17-._.js","static/chunks/node_modules_10e2-xo._.js","static/chunks/[root-of-the-server]__0pxj09-._.js"],
+    source: "entry"
+});
---
+++ vercel-ai-summary/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_14ibvna._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js",
+  "static/chunks/node_modules_next_dist_client_16lnmlo._.js",
+  "static/chunks/node_modules_next_dist_0zk-zb1._.js",
+  "static/chunks/1qf1_next_dist_pages__error_0f1i7ci.js",
+  "static/chunks/[next]_entry_page-loader_ts_02c0ybi._.js",
+  "static/chunks/node_modules_react-dom_0kla17-._.js",
+  "static/chunks/node_modules_10e2-xo._.js",
+  "static/chunks/[root-of-the-server]__0pxj09-._.js",
+  "static/chunks/[project]_vercel-ai-summary_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_vercel-ai-summary_pages__error_1y_40z6._.js"
+])
